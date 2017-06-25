76dc27cc857f5d730a323f7babee997747e4a6ee
fecabianchi <felipecabianchi@gmail.com>
2017-06-24 21:00:44 -0300

    jam coin added

---
+++ jam_coin/helpers.js
@@ -0,0 +1,27 @@
+function toBase(bin, base) {
+  const binary = bin.reverse()
+  let result = []
+  let data = 0
+  let convert = 0
+  for(let i = 0; i < binary.length; i++) {
+    convert = binary[i] * Math.pow(base, i)
+    result.push(convert)
+  }
+  data = result.reduce((cur, next) => cur + next)
+  return data
+}
+
+function trialDivisionTest(n) {
+  const sqrt = Math.sqrt(n)
+  for(var i = 2; i <= 10; ++i) {
+    if (n % i === 0) {
+      return false
+    }
+  }
+  return true
+}
+
+module.exports = {
+  trialDivisionTest,
+  toBase
+}
---
+++ jam_coin/jam_coin.js
@@ -0,0 +1,36 @@
+require('babel-polyfill')
+const itertools = require('aureooms-js-itertools')
+const helpers = require('./helpers')
+
+function jam_coin() {
+  const gen = itertools.product( [ "01"] ,  30);
+  let coins = 0;
+
+  while (true) {
+    let bin = gen.next().value
+    bin.unshift(1)
+    bin.push(1)
+
+    let stop = false
+
+    for(let i = 2; i < 11; i++) {
+      let converted = helpers.toBase(bin, i)
+      if(helpers.trialDivisionTest(converted)) {
+        stop = true;
+        break;
+      }
+    }
+
+    if(stop) {
+      continue;
+    } else {
+      coins += 1
+      console.log("Case #" + coins + ": " + bin.join(''))
+      if (coins >= 500) {
+        break
+      }
+    }
+  }
+}
+
+jam_coin()
